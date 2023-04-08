9fd8bc63ea21107e9ffda6314817e034a7461fe6
Rizky Rahman <rizkpython87@gmail.com>
2023-04-09 01:37:53 +0800

    Aplikasi Komplit

--- gempaterkini/__init__.py
+++ gempaterkini/__init__.py
@@ -25,13 +25,39 @@ def akstraksi_data():
         tanggal = result[0]
         waktu = result[1]
 
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+        i = 0
+        magnitudo = None
+        kedalaman = None
+        ls  = None
+        bt = None
+        lokasi = None
+        tsunami = None
+
+        for res in result:
+            if i == 1:
+                magnitudo = res.text
+            elif i == 2:
+                kedalaman = res.text
+            elif i == 3:
+                koordinat = res.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif i == 4:
+                lokasi = res.text
+            elif i == 5:
+                tsunami = res.text
+            i = i + 1
+
         hasil = dict()
-        hasil["tanggal"] = tanggal     #"24 Agustus 2021"
-        hasil["waktu"] = waktu    #"12:05:52 WIB"
-        hasil["magnitudo"] = 4.0
-        hasil["lokasi"] = {"ls": 1.48, "bt": 134.01}
-        hasil["pusat"] = "Pusat gempa berada di darat 18 km barat laut Ransiki"
-        hasil["dirasakan"] = "Dirasakan (Skala MMI): II-III Manokwari, II-III Ransiki"
+        hasil["tanggal"] = tanggal
+        hasil["waktu"] = waktu
+        hasil["magnitudo"] = magnitudo
+        hasil["kedalaman"] = kedalaman
+        hasil["koordinat"] = {"ls": ls, "bt": bt}
+        hasil["lokasi"] = lokasi
+        hasil["tsunami"] = tsunami
         return hasil
     else:
         return None
@@ -43,13 +69,15 @@ def tampilkan_data(result):
     if result is None:
         print('Tidak bisa menemukan data gempa terkini')
         return
+
     print("Gempa Terakhir berdasarkan BMKG")
     print(f"Tanggal {result['tanggal']}")
     print(f"Waktu {result['waktu']}")
     print(f"Magnitudo {result['magnitudo']}")
-    print(f"Lokasi: LS={result['lokasi']['ls']}, BT={result['lokasi']['bt']}")
-    print(f"Pusat {result['pusat']}")
-    print(f"Dirasakan {result['dirasakan']}")
+    print(f"Kedalaman {result['kedalaman']}")
+    print(f"Koordinat: LS={result['koordinat']['ls']}, BT={result['koordinat']['bt']}")
+    print(f"Lokasi {result['lokasi']}")
+    print(f"Tsunami {result['tsunami']}")
 
 if __name__ == '__main__':
     print('Ini adalah package gempaterkini')
